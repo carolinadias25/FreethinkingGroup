de01dac12bd8295a486303c69d73fc6969ac6dcb
Carolina <carolina@doom.local>
2021-11-09 12:39:39 +0000

    deploying

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,41 +1,31 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
-    //
-    public function contact(){
+    public function contact()
+    {
         return view('contact');
     }
 
-    Public function contactSubmit( Request $request){
-//        dd($request->input("msg"));
-//        $name = $request->input("name");
-//        $request->validate([
-//            'name' => 'required|min:4',
-//            'email' => 'required|email',
-//            'phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
-//            'message' => 'required|max:255'
-//        ]);
-
+    Public function contactSubmit( Request $request)
+    {
         $data = array(
             'name' => $request->input("name"),
             'email' => $request->input("email"),
             'phone'=> $request->input("phone"),
             'msg' => $request->input("msg")
         );
-        Mail::send('emails.contactmail', $data, function ($mail) use ($request){
-            $mail->from(env('MAIL_FROM_ADDRESS', 'The Freethinking Group'));
+
+        Mail::send('emails.contactEmail', $data, function ($mail) use ($request){
+            $mail->from('carolinad@intelligence22.com');
             $mail->to("carolina.f.dias@hotmail.com")->subject('Contact Message');
         });
+
         toastr()->success('Your message has been sent successfully');
-
-        return back();
-
+        return redirect()->back();
     }
 }
