1f13bf71deac2634cdca121d9cbf9ff77120626d
Carolina <carolina@doom.local>
2021-11-09 15:02:18 +0000

    deploying-fixing contact form error

--- config/mail.php
+++ config/mail.php
@@ -1,6 +1,21 @@
 <?php
 
 return [
+    /*
+|--------------------------------------------------------------------------
+| Mail Driver
+|--------------------------------------------------------------------------
+|
+| Laravel supports both SMTP and PHP's "mail" function as drivers for the
+| sending of e-mail. You may specify which one you're using throughout
+| your application here. By default, Laravel is setup for SMTP mail.
+|
+| Supported: "smtp", "sendmail", "mailgun", "ses",
+|            "postmark", "log", "array"
+|
+*/
+
+    'driver' => env('MAIL_DRIVER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
